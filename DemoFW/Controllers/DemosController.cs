ef94a2dbc9c456b43997af20881c93345da61f3b
Javier <jmartin@grupoloyal.com>
2023-09-25 14:02:05 +0200

    Basica

---
+++ DemoFW/Controllers/DemosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoFW.Controllers
+{
+    public class DemosController : Controller
+    {
+        // GET: Demos
+        public ActionResult Index(int? id, string nom = "mundo")
+        {
+            ViewData["nombre"] = nom;
+            ViewBag.despide = id > 0;
+            ViewBag.lista = new string[] { "uno", "dos", "tres" };
+            if(ViewBag.despide) {
+                //return View("Adios");
+                // return this.HttpNotFound();
+                return this.Json(ViewData);
+            }
+            return View();
+        }
+    }
+}
