c304789afb15357fdd0c0ff97a304f62b061704c
Javier <jmartin@grupoloyal.com>
2023-09-29 14:14:37 +0200

    Core

---
+++ DemoMVCCore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using DemoMVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoMVCCore.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            ViewBag.algo = "kk";
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            ViewBag.algo = "kk";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
